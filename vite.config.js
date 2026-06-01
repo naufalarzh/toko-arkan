213874c6a99ebe0ffe7338f75699ab5c95cf8796
naufalarzh <zhfrn81@gmail.com>
2026-06-01 22:35:31 +0700

    cape

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  }
-})
+  plugins: [react(), tailwindcss()],
+});
