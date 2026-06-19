d8ca3d6a15652f878111baed7a01af6d43fb16fb
naufalarzh <zhfrn81@gmail.com>
2026-06-19 21:57:39 +0700

    delete makanan dan minuman

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 import noPictures from "../assets/no-pictures.png";
 
-export const LIST_KATEGORI = ["Semua", "Sembako", "Makanan", "Minuman", "Rokok", "Alat Tulis", "Listrik", "Lainnya"];
+export const LIST_KATEGORI = ["Semua", "Sembako", "Rokok", "Alat Tulis", "Listrik", "Lainnya"];
 
 export const DEFAULT_IMAGE = noPictures;
